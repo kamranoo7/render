c80bae7802c7bdb6467f4e252d947a1563d246aa
MD Kamran khan <mohammadkkhan78@gmail.com>
2023-07-02 10:20:34 +0530

    ss

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-
+let port=process.env.port
 server.use(middlewares)
 server.use(router)
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('JSON Server is running')
 })
